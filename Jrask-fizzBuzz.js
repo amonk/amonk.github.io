620f3a5fd3e41ab5be1af0ea4329e78172da508a
amonk <amokamonk@gmail.com>
2020-12-01 23:53:25 -0600

    First init...

---
+++ Jrask-fizzBuzz.js
@@ -0,0 +1,50 @@
+// To run in node navigate to file and run the following in your terminal
+// node Jrask-fizzBuzz.js
+
+//Fizzbuzz
+const fizzBuzz = () => {
+	for(let num = 1; num <= 100; num++){
+		if(num % 3 === 0 && num % 5 === 0) {
+			console.log('FizzBuzz')
+		}else if(num % 5 === 0) {
+			console.log('Buzz')
+		}else if(num % 3 === 0) {
+			console.log('Fizz')
+		}else {
+			console.log(num)
+		}
+	}
+}
+
+const fizzBuzzArray = () => {
+    const out = []
+	for(let num = 1; num <= 100; num++){
+		if(num % 3 === 0 && num % 5 === 0) {
+			out.push('FizzBuzz')
+		}else if(num % 5 === 0) {
+			out.push('Buzz')
+		}else if(num % 3 === 0) {
+			out.push('Fizz')
+		}else {
+			out.push(num)
+		}
+    }
+    return out
+}
+
+const fizzBuzzHTML = () => {
+    const out = []
+	for(let num = 1; num <= 100; num++){
+		if(num % 3 === 0 && num % 5 === 0) {
+			document.write('FizzBuzz </br>')
+		}else if(num % 5 === 0) {
+			document.write('Buzz </br>')
+		}else if(num % 3 === 0) {
+			document.write('Fizz </br>')
+		}else {
+			document.write(`${num} </br>`)
+		}
+    }
+}
+fizzBuzz()
+console.log('fizBuzzArray', fizzBuzzArray())
